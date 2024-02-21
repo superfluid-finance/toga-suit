e513280dcf351e67169550c11b0157a146b1b08c
Kaspar Kallas <kaspar@superfluid.finance>
2024-02-21 19:26:27 +0200

    sync with the dashboard

--- toga-dashboard/src/components/wallet/getAppWallets.js
+++ toga-dashboard/src/components/wallet/getAppWallets.js
@@ -1,50 +1,43 @@
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import {
-    connectorsForWallets,
-  } from "@rainbow-me/rainbowkit";
-  import {
-    metaMaskWallet,
-    braveWallet,
-    walletConnectWallet,
-    coinbaseWallet,
-    rainbowWallet,
-  } from "@rainbow-me/rainbowkit/wallets";
-  import gnosisSafe from "./gnosisSafeWalletConnector/gnosisSafe";
-  import mockConnector from "./mockConnector/mockConnector";
-  import { namedInjectedWallet } from "./namedInjectedWallet/namedInjectedWallet";
-  
-  // Inspired by: https://github.com/rainbow-me/rainbowkit/blob/main/packages/rainbowkit/src/wallets/getDefaultWallets.ts
-  export const getAppWallets = ({
-    appName,
-    chains,
-    projectId,
-  }) => {
-    const needsMock =
-      typeof window !== "undefined" &&
-      typeof (window).mockSigner !== "undefined";
-  
-    const wallets = [
-      {
-        groupName: "Popular",
-        wallets: [
-          namedInjectedWallet({ chains, shimDisconnect: true }),
-          gnosisSafe({ chains, projectId}),
-          braveWallet({ chains, shimDisconnect: true }),
-          metaMaskWallet({
-            chains,
-            shimDisconnect: true,
-            projectId: projectId
-          }),
-          walletConnectWallet({ chains, projectId, showQrModal: true }),
-          coinbaseWallet({ appName, chains }),
-          rainbowWallet( { chains, projectId }),
-          ...(needsMock ? [mockConnector({ chains })] : []),
-        ],
-      },
-    ];
-  
-    return {
-      connectors: connectorsForWallets(wallets),
-      wallets,
-    };
-  };
-  
+	metaMaskWallet,
+	braveWallet,
+	walletConnectWallet,
+	coinbaseWallet,
+	rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import gnosisSafe from './gnosisSafeWalletConnector/gnosisSafe';
+import mockConnector from './mockConnector/mockConnector';
+import { namedInjectedWallet } from './namedInjectedWallet/namedInjectedWallet';
+
+// Inspired by: https://github.com/rainbow-me/rainbowkit/blob/main/packages/rainbowkit/src/wallets/getDefaultWallets.ts
+export const getAppWallets = ({ appName, chains, projectId }) => {
+	const needsMock =
+		typeof window !== 'undefined' &&
+		typeof window.mockSigner !== 'undefined';
+
+	const wallets = [
+		{
+			groupName: 'Popular',
+			wallets: [
+				namedInjectedWallet({ chains, shimDisconnect: true }),
+				gnosisSafe({ chains, projectId }),
+				braveWallet({ chains, shimDisconnect: true }),
+				metaMaskWallet({
+					chains,
+					shimDisconnect: true,
+					projectId: projectId,
+				}),
+				walletConnectWallet({ chains, projectId, showQrModal: true }),
+				coinbaseWallet({ appName, chains }),
+				rainbowWallet({ chains, projectId }),
+				...(needsMock ? [mockConnector({ chains })] : []),
+			],
+		},
+	];
+
+	return {
+		connectors: connectorsForWallets(wallets)(),
+		wallets,
+	};
+};
